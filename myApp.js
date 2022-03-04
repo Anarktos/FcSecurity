97b16f8ce50354e030a7b9972c6d8b68fb873f48
Anarktos <camilowhite7@gmail.com>
2022-03-04 11:41:09 -0500

    helmet

--- myApp.js
+++ myApp.js
@@ -15,6 +15,7 @@ app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({directives:{defaultSrc:["'self'"], scriptSrc: ["'self'"]}}));
 
 
 
